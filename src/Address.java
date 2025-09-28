8d0584cff6a3c63fb018913e4ccea8bc31f6538c
MylesBraverman <msbrav26@colby.edu>
2025-09-28 18:36:49 -0400

    Created basic Address class with fields and a single method to set all fields at once. Did not create seperate setters because individual aspects of addresses are never modified in the project implementation. They are only ever set all at once

---
+++ src/Address.java
@@ -0,0 +1,18 @@
+public class Address {
+    private String line1;
+    private String line2;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+
+    public void updateInfo(String line1, String line2, String city, String state, String zipCode, String country){
+        this.line1 = line1;
+        this.line2 = line2;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+}
