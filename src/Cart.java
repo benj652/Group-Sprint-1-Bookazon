24257e88f0b9d61c366c090828c9f46461bd4204
derrick-code10 <demoemail1428@gmail.com>
2025-09-27 22:35:49 -0400

    added dependency inversion

--- src/Cart.java
+++ src/Cart.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Cart {
-    private ArrayList<CartItem> items;
-    
+    private List<CartItem> items;
+
     public Cart() {
-        items = new ArrayList<>();
+        this(new ArrayList<>());
     }
-    
+
+    public Cart(List<CartItem> items) {
+        this.items = items;
+    }
+
     public void addItem(CartItem item) {
         items.add(item);
     }
@@ -14,7 +19,7 @@ public class Cart {
     public void removeItem(CartItem item) {
         items.remove(item);
     }
-    
+
     public void updateQuantity(CartItem item, int quantity) {
         for (CartItem cartItem : items) {
             if (cartItem.equals(item)) {
@@ -23,7 +28,7 @@ public class Cart {
             }
         }
     }
-    
+
     public void viewCartDetails() {
         System.out.println("Cart Details:");
         for (CartItem item : items) {
@@ -31,8 +36,8 @@ public class Cart {
         }
         System.out.println("\n");
     }
-    
-    public ArrayList<CartItem> getItems() {
+
+    public List<CartItem> getItems() {
         return items;
     }
 }
