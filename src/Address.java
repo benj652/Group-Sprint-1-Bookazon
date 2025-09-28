61580892e23f20137e962347a1a5ed8e774ee6df
MylesBraverman <msbrav26@colby.edu>
2025-09-28 18:46:02 -0400

    Added a toString method to Address to simplify the printing in other classes

--- src/Address.java
+++ src/Address.java
@@ -7,12 +7,18 @@ public class Address {
     private String country;
 
 
-    public void updateInfo(String line1, String line2, String city, String state, String zipCode, String country){
+    public void setAllFields(String line1, String line2, String city, String state, String zipCode, String country){
         this.line1 = line1;
         this.line2 = line2;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
         this.country = country;
+    }
+
+    public String toString(){
+        String addressString = this.line1 + "," + this.line2 + "," + this.city + "," 
+        + this.state + "," + this.zipCode + "," + this.country; 
+        return addressString;
     }
 }
