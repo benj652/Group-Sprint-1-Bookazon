dcdd82e4de04b2c7dcff5b9249ded587d9034b2f
MylesBraverman <msbrav26@colby.edu>
2025-09-28 19:03:36 -0400

    updated User.java to use Address class for its shipping and billing address

--- src/User.java
+++ src/User.java
@@ -6,24 +6,16 @@ public class User {
     private String subscription;
     private Cart cart;
     private ArrayList<Order> orders;
-    private String shippingAddressLine1;
-    private String shippingAddressLine2;
-    private String shippingAddressCity;
-    private String shippingAddressState;
-    private String shippingAddressZip;
-    private String shippingAddressCountry;
-    private String billingAddressLine1;
-    private String billingAddressLine2;
-    private String billingAddressCity;
-    private String billingAddressState;
-    private String billingAddressZip;
-    private String billingAddressCountry;
+    private Address shippingAddress;
+    private Address billingAddress;
 
     public User(String name, String subscription) {
         this.name = name;
         this.subscription = subscription;  // normal, gold, platinum, silver membership
         this.cart = new Cart();
         this.orders = new ArrayList<>();
+        this.shippingAddress = new Address();
+        this.billingAddress = new Address();
     }
 
     public String getName() {
@@ -43,21 +35,11 @@ public class User {
     }
 
     public void setShippingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.shippingAddressLine1 = line1;
-        this.shippingAddressLine2 = line2;
-        this.shippingAddressCity = city;
-        this.shippingAddressState = state;
-        this.shippingAddressZip = zip;
-        this.shippingAddressCountry = country;
+        shippingAddress.setAllFields(line1, line2, city, state, zip, country);
     }
 
     public void setBillingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.billingAddressLine1 = line1;
-        this.billingAddressLine2 = line2;
-        this.billingAddressCity = city;
-        this.billingAddressState = state;
-        this.billingAddressZip = zip;
-        this.billingAddressCountry = country;
+        billingAddress.setAllFields(line1, line2, city, state, zip, country);
     }
 
     public void addToCart(Book book, int quantity) {
