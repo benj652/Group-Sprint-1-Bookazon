dccd7cc6fcfbdeb1c9ba3cabea8d3a0fa208bfb3
MylesBraverman <msbrav26@colby.edu>
2025-09-28 20:37:17 -0400

    Created Product class

---
+++ src/Product.java
@@ -0,0 +1,73 @@
+public abstract class Product {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private double price;
+    private String mediaType;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public void printProductDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("Price: $" + price);
+        System.out.println("Media Type: " + mediaType);
+    }
+
+    public boolean isPriceValid() {
+        return price > 0;
+    }
+
+    public boolean isTitleValid() {
+        return title != null && !title.isEmpty();
+    }
+
+    public boolean isAuthorValid() {
+        return author != null && !author.isEmpty();
+    }
+
+    public boolean isYearPublishedValid() {
+        return yearPublished > 0;
+    }
+}
+
