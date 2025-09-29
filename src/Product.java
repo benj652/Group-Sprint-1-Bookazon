f4b15e664d5bd1e711ae432da36d5909cc2ecc5d
MylesBraverman <msbrav26@colby.edu>
2025-09-28 21:03:20 -0400

    Replaced Book class with PaperbackBook class and HardcoverBook class, both of  which extend Product

--- src/Product.java
+++ src/Product.java
@@ -54,6 +54,13 @@ public abstract class Product {
         System.out.println("Media Type: " + mediaType);
     }
 
+    public void setFields(String title, String author, int yearPublished, double price){
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+
     public boolean isPriceValid() {
         return price > 0;
     }
